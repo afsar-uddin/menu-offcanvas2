597e74b42d4086acb12f8c0666e3ef4510b1be6f
Md Afsar Uddin <afsarwebdev@gmail.com>
2018-02-07 01:07:29 +0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,14 @@
+
+
+function openNav() {
+	document.getElementById("mySideNav").style.width = "250px";
+}
+
+function closeNav() {
+	document.getElementById("mySideNav").style.width = "0";
+}
+
+
+
+
+ 
